b747f1090928b4d4da8ccbc1591b9aaa74ae6eef
Mailyn Sajorda <mailyn.sajorda@englishcentral.com>
2019-06-03 20:51:07 +0800

    Added lesson type object.

---
+++ ECTutorKit/ECLessonType.swift
@@ -0,0 +1,13 @@
+//
+//  ECLessonType.swift
+//  ECTutorKit
+//
+//  Created by Mailyn Sajorda on 03/06/2019.
+//  Copyright © 2019 Mailyn Sajorda. All rights reserved.
+//
+
+import UIKit
+
+class ECLessonType: NSObject {
+
+}
